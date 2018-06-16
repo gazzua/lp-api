ee893fae016cee9680554cb4c4ef02dd6ffae74b
Minsub Kim <minsubkim94@gmail.com>
2018-06-16 20:30:47 +0900

    feat: add entities repository
    Fixes #13

---
+++ src/repositories/CommentRepository.ts
@@ -0,0 +1,10 @@
+import {
+  EntityRepository, 
+  TreeRepository,
+} from 'typeorm';
+import Comment from '@entities/Comment';
+
+@EntityRepository(Comment)
+export class CommentRepository extends TreeRepository<Comment> {
+
+}
