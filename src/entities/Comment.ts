195cb755ab90d5a4234bfedda8eeac518dc3af4b
Minsub Kim <minsubkim94@gmail.com>
2018-06-17 04:04:35 +0900

    fix: comment is not showing user entity
    Fixes https://github.com/language-project/lp-api/issues/34

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -34,7 +34,9 @@ import {
     @Column()
     public targetId: number;
   
-    @ManyToOne((type) => User)
+    @ManyToOne((type) => User, {
+      eager: true,
+    })
     @JoinColumn({
       name: 'userId',
     })
